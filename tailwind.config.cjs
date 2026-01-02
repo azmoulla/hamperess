b460869cf802aad6fec138116bd0a10221375e92
Az <az.moulla@gmail.com>
2026-01-02 17:30:25 +0000

    Fix netlify.toml publish path to root

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",          // <--- Scans your main HTML file
-    "./app.js",              // <--- Scans your main JS file
-    "./public/**/*.{html,js}" // <--- Scans any extra files in public
+    "./index.html",            // Scans index.html in Root
+    "./app.js",                // Scans app.js in Root
+    "./public/**/*.{html,js}"  // Scans any leftovers in public
   ],
   theme: {
     extend: {},
