8483c26307917dbd6be56daae12b814ae905c2e4
azmoulla <az.moulla@gmail.com>
2026-01-02 23:19:25 +0000

    Update Tailwind CSS config content paths

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 content: [
-  "./index.html",
-  "./**/*.{html,js}"
-],
+    "./index.html",          // 1. Matches index.html in the root
+    "./*.js",                // 2. Matches all JS files in the root (app.js, auth.js, etc.) but NOT subfolders
+    "./public/**/*.html"     // 3. Matches any HTML files still inside public
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 
 }
+
 
