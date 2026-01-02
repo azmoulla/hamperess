f18ec0f435795aeee7acc8dff01d8ee54fa5eefb
azmoulla <az.moulla@gmail.com>
2026-01-02 23:59:42 +0000

    Expose Firebase and Algolia config to global scope

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
-// FILE: public/firebase-config.js
-const firebaseConfig = {
+// We use 'window.' to make these variables public to the whole app
+window.firebaseConfig = {
   apiKey: "AIzaSyBzU9YCpen0fJ12eGSnLeQGXsavSa9kX3w",
   authDomain: "luxury-hampers-app.firebaseapp.com",
   projectId: "luxury-hampers-app",
@@ -8,5 +8,6 @@ const firebaseConfig = {
   appId: "1:314612428903:web:3dca1b32342818124c2",
   measurementId: "G-LXPLK738BM"
 };
-const ALGOLIA_APP_ID = '2HPG7X4SK4'; // From your .env file
-const ALGOLIA_SEARCH_KEY = 'c95f601b8e003e4a88624f0e3a1d8ad7'; // Your SEARCH-ONLY key
+
+window.ALGOLIA_APP_ID = '2HPG7X4SK4';
+window.ALGOLIA_SEARCH_KEY = 'c95f601b8e003e4a88624f0e3a1d8ad7';
